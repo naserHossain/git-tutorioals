f02682ff37a46fd1c3e666e78132c37312845784
shawon <shawon.softeng@gmail.com>
2023-03-18 21:14:58 +0600

    commited

--- hello.java
+++ hello.java
@@ -1,10 +1,10 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Visual studio code is awesome")\
-        greeting("git")
+        greeting("hello I am from branch A")
     }
 
     public static void greeting(String name){
-        System.out.println("Bye Bye " + name)
+        System.out.println("Hello branch A " + name)
     }
 }
